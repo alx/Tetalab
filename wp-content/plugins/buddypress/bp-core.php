fda91c345dd743e94d01fd4c33fb20857acd2722
Alexandre Girard <alx.girard@gmail.com>
2009-10-17 19:38:22 +0200

    change slug to register page

--- wp-content/plugins/buddypress/bp-core.php
+++ wp-content/plugins/buddypress/bp-core.php
@@ -52,7 +52,7 @@ if ( !defined( 'BP_MEMBERS_SLUG' ) )
 
 /* Define the slug for the register/signup page */
 if ( !defined( 'BP_REGISTER_SLUG' ) )
-	define( 'BP_REGISTER_SLUG', 'register' );
+	define( 'BP_REGISTER_SLUG', 'wp-signup.php' );
 
 /* Define the slug for the activation page */
 if ( !defined( 'BP_ACTIVATION_SLUG' ) )
