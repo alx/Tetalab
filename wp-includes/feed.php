733e6957d4a44d0b6823868dd0bfe6d2cebe78cb
Alexandre Girard <alx.girard@gmail.com>
2010-01-18 17:55:02 +0100

    add debug

--- wp-includes/feed.php
+++ wp-includes/feed.php
@@ -192,9 +192,8 @@ function the_permalink_rss() {
 	if(strlen($link) == 0){
 		$link = get_permalink();
 	}
-	echo "post: ".$post->ID;
-	echo "get_metadata: ".get_metadata('post', $post->ID, 'wpmu-link', true);
-	echo apply_filters('the_permalink_rss', get_metadata('post', $post->ID, 'wpmu-link', true));
+	echo apply_filters('the_permalink_rss', $post->ID);
+	//echo apply_filters('the_permalink_rss', get_metadata('post', $post->ID, 'wpmu-link', true));
 	//echo apply_filters('the_permalink_rss', get_permalink());
 }
 
