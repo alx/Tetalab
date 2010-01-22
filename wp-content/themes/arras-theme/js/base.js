d0057b45585a95ac9eb063ab0e2fcac06f4193a2
Alexandre Girard <alx.girard@gmail.com>
2010-01-22 01:04:30 +0100

    correct base.js

--- wp-content/themes/arras-theme/js/base.js
+++ wp-content/themes/arras-theme/js/base.js
@@ -1,30 +1,11 @@
-jQuery(document).ready(function($) {
-	$('#multi-sidebar').tabs();
-	
-	$('#commentform').validate();
-	$('.featured').hover( 
-		function() {
-			$('#featured-slideshow').cycle('pause');
-			$('#controls').fadeIn();
-		}, 
-		function() {
-			$('#featured-slideshow').cycle('resume');
-			$('#controls').fadeOut();
-		}
-	);
-	$('#featured-slideshow').cycle({
-		fx: 'fade',
-		speed: 250,
-		next: '#controls .next',
-		prev: '#controls .prev',
-		timeout: 6000
-	});
-	
-	$('#show_projects').click(function(){
-		if($("#more_projects").is(':visible')){
-			$("#more_projects").hide("slide", { direction: "down" }, 1000);
+var j = jQuery.noConflict();
+
+j(document).ready(function() {
+	j('#show_projects').click(function(){
+		if(j("#more_projects").is(':visible')){
+			j("#more_projects").hide("slide", { direction: "down" }, 1000);
 		} else {
-			$("#more_projects").show("slide", { direction: "down" }, 1000);
+			j("#more_projects").show("slide", { direction: "down" }, 1000);
 		}
 		
 	});
