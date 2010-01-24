becc790d588749723e8b0dc4ff4c87eaa8f102c9
Alexandre Girard <alx.girard@gmail.com>
2010-01-24 16:49:45 +0100

    animate slide

--- wp-content/themes/arras-theme/js/base.js
+++ wp-content/themes/arras-theme/js/base.js
@@ -6,10 +6,10 @@ j(document).ready(function() {
 		var img = j('#more_projects_tab img').attr('src');
 		
 		if(img.search(/up/) != -1){
-			j("#more_projects").effect("slide", { direction: "up" }, 300);
+			j("#more_projects").animate({top: "-=1000px"}, 300);
 			j('#more_projects_tab img').attr('src', img.replace("up", "down"));
 		} else {
-			j("#more_projects").effect("slide", { direction: "down" }, 300);
+			j("#more_projects").animate({top: "+=1000px"}, 300);
 			j('#more_projects_tab img').attr('src', img.replace("down", "up"));
 		}
 		
