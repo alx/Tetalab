105e1e7a0669909b762b5b18bf3accaaaffee298
Alexandre Girard <alx.girard@gmail.com>
2010-02-06 14:21:36 +0100

    change image size and use gallery id

--- wp-content/themes/arras-theme/js/base.js
+++ wp-content/themes/arras-theme/js/base.js
@@ -15,9 +15,9 @@ j(document).ready(function() {
 		
 	});
 	
-	j('#photos').galleryView({
-		panel_width: 1200,
-		panel_height: 400,
+	j('#gallery').galleryView({
+		panel_width: 900,
+		panel_height: 300,
 		frame_width: 100,
 		frame_height: 100
 	});
