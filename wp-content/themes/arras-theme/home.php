be2f4868444a79549f13b6e939da3076994d7f36
Alexandre Girard <alx.girard@gmail.com>
2010-02-17 21:29:02 +0100

    remove tetaglobule

--- wp-content/themes/arras-theme/home.php
+++ wp-content/themes/arras-theme/home.php
@@ -56,12 +56,6 @@ $stickies = get_option('sticky_posts');
 	<div class="home-title">Videos</div>
 	<?php get_video_posts(); ?>
 </div><!-- #index-featured -->
-
-<div id="index-tetaglobule">
-	<div class="video-link"><a href="http://vimeo.com/channels/tetaglobule">Plus de Videos &#x2192;</a></div>
-	<div class="home-title">Coups de Coeurs</div>
-	<?php get_video_posts('channel', 'post'); ?>
-</div><!-- #index-tetaglobule -->
 <?php endif; endif; ?>
 
 
