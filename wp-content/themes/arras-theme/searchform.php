0607b95a9f1ff5f5564149ea9a1e7c7031d65821
Alexandre Girard <alx.girard@gmail.com>
2010-02-22 09:43:50 +0100

    remove arras theme

--- wp-content/themes/arras-theme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" class="searchform clearfix" action="<?php bloginfo('url'); ?>/">
- <input type="text" value="<?php the_search_query(); ?>" name="s" class="s" onfocus="this.value=''" />
- <input type="submit" class="searchsubmit" value="<?php _e('Search', 'arras') ?>" title="<?php printf( __('Search %s', 'arras'), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-</form>
