0fe6a7818587303f220f9cb4676a98117953eebb
Alexandre Girard <alx.girard@gmail.com>
2010-02-25 01:01:38 +0100

    use static footer

--- wp-content/themes/tetalab/footer.php
+++ wp-content/themes/tetalab/footer.php
@@ -1,26 +1,30 @@
 	<?php arras_before_footer() ?>
 	</div><!-- #main -->
-    
     <div id="footer" class="clearfix">
-    	<ul id="footer-sidebar" class="clearfix xoxo">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>
-            <?php endif; ?>
-        </ul>
+		<ul id="footer-sidebar" class="clearfix xoxo">
+			<li class="widgetcontainer clearfix">
+				<h5 class="widgettitle">Hackerspaces.org</h5>
+				<div class="widgetcontent">
+					<div class="textwidget">
+						<a href="http://hackerspaces.org">
+							<img src="/wp-content/themes/tetalab/images/hackerspaces.png" width="150px" height="85px" alt="hackerspaces.org"/>
+						</a>
+					</div>
+				</div>
+			</li>
+		</ul>
 		<ul class="footer-message">
 			<li class="widgetcontainer">
-                	<h4 class="widgettitle"><?php echo stripslashes(arras_get_option('footer_title')); ?></h4>
-                	<div class="widgetcontent">
-                	<?php echo stripslashes(arras_get_option('footer_message')); ?>
-					
-                	<p>Theme: <a href="http://www.arrastheme.com/"><strong>Arras</strong></a></p>
-                	</div>
-            </li>
+				<h4 class="widgettitle">Tetalab - Toulouse HackerSpace</h4>
+				<div class="widgetcontent">
+					<p>Come and play!<br/>
+					Theme: <a href="http://www.arrastheme.com/"><strong>Arras</strong></a></p>
+				</div>
+			</li>
 		</ul>
-    </div>
-    
-    <?php wp_footer() ?>
+	</div>
+	<?php wp_footer() ?>
 	<?php arras_footer() ?>
-
 </div><!-- #wrapper -->
 </body>
 </html>
