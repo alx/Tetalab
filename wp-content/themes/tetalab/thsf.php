74cceb139c5c231d3e4c37c2d90957a375eaf939
Alexandre Girard <alx.girard@gmail.com>
2010-05-17 14:23:34 +0200

    remove table content on planning page

--- wp-content/themes/tetalab/thsf.php
+++ wp-content/themes/tetalab/thsf.php
@@ -17,7 +17,9 @@ else add_filter('arras_postheader', 'arras_postmeta');
         <?php arras_postheader() ?>
         
         <div class="entry-content">
-		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>
+		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>  
+        <?php wp_link_pages(array('before' => __('<p><strong>Pages:</strong> ', 'arras'), 
+			'after' => '</p>', 'next_or_number' => 'number')); ?>
 		</div>
         
         <!-- <?php trackback_rdf() ?> -->
