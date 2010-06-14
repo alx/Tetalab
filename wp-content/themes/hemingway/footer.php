1f164aaeddbf2f723fa56e6faf31ef9154c273ba
Alexandre Girard <alx.girard@gmail.com>
2010-06-14 10:18:14 +0200

    add hemingway theme

---
+++ wp-content/themes/hemingway/footer.php
@@ -0,0 +1,16 @@
+
+<hr class="hide" />
+	<div id="footer">
+		<div class="inside">
+			<?php
+				// You are not required to keep this link back to Warpspire, but if you wouldn't mind, leaving it in would make my day.
+			?>
+			<p class="copyright">Powered by <a href="http://warpspire.com/hemingway" rel="designer">Hemingway</a> flavored <a href="http://wordpress.org">Wordpress</a>.</p>
+			<p class="attributes"><a href="<?php bloginfo('rss2_url'); ?>">Entries RSS</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments RSS</a></p>
+		</div>
+	</div>
+	<!-- [END] #footer -->	
+
+	<?php wp_footer(); ?>
+</body>
+</html>
